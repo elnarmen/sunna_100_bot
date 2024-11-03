eccab99e1f91b215909d22dcc185acbb1e84ff7e
Эльнар <elnar_men@mail.ru>
2024-11-04 02:10:30 +0300

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,3 @@ git pull
 docker-compose -f docker-compose.prod.yaml down
 docker-compose -f docker-compose.prod.yaml up -d
 docker exec sto_sunn_web python /app/sto_sunn/manage.py migrate --no-input
-# docker exec sto_sunn_web python /app/sto_sunn/manage.py run_bot
