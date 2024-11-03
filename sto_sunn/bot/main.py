ffd93fcbb43ab7a33936d176d7ab093f30b8815e
Эльнар <elnar_men@mail.ru>
2024-11-03 17:40:08 +0300

    Add logging

--- sto_sunn/bot/main.py
+++ sto_sunn/bot/main.py
@@ -1,4 +1,5 @@
 import itertools
+import logging
 import os
 import re
 import time
@@ -7,7 +8,20 @@ import telegram
 from bot.models import Interval, Post
 from dotenv import load_dotenv
 
+logger = logging.getLogger("bot_logger")
+
 load_dotenv()
+
+
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, bot, chat_id):
+        super().__init__()
+        self.bot = bot
+        self.chat_id = chat_id
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def escape_markdown_v2(text):
@@ -32,13 +46,11 @@ def run_bot():
     bot = telegram.Bot(token=os.getenv("TG_TOKEN"))
     chat_id = os.getenv("CHAT_ID")
     chat_id_for_logs = os.getenv("CHAT_ID_FOR_LOGS")
-    posts = Post.objects.all()
-    interval = (
-        Interval.objects.first()
-    )  # set 24 hours by default if there is no Interval in the db
-    hours_between_posts = interval.hours if interval else 24
-    seconds_between_posts = hours_between_posts * 3600
 
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(bot, chat_id_for_logs))
+
+    posts = Post.objects.all()
     if not posts.exists():
         bot.send_message(
             chat_id=chat_id_for_logs,
@@ -46,14 +58,17 @@ def run_bot():
         )
         return
 
-    posts_cycle = itertools.cycle(posts)
+    interval = (
+        Interval.objects.first()
+    )  # set 24 hours by default if there is no Interval in the db
+    hours_between_posts = interval.hours if interval else 24
+    seconds_between_posts = hours_between_posts * 3600
 
+    posts_cycle = itertools.cycle(posts)
     for post in posts_cycle:
         try:
             send_post(bot, chat_id, post)
             time.sleep(seconds_between_posts)
-        except Exception as e:
-            bot.send_message(
-                chat_id=chat_id_for_logs,
-                text=f"An error occurred: {e}. PostID - {post.id}",
-            )
+        except Exception as err:
+            logger.exception(err)
+            time.sleep(seconds_between_posts)
