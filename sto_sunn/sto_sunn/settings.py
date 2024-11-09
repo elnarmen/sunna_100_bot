99a5c826454c2a863947c2ace434a0a72e9b3631
Эльнар <elnar_men@mail.ru>
2024-11-09 11:30:26 +0300

    Fix settings

--- sto_sunn/sto_sunn/settings.py
+++ sto_sunn/sto_sunn/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": "db",
+        "HOST": os.getenv("HOST"),
     }
 }
 
